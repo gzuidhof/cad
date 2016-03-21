1d910609915b9b2b60a4292f1f6ffd655a77ab54
Guido <me@guido.io>
2016-03-21 11:52:44 +0100

    Remove completely black feature vectors

--- src/rebalance.py
+++ src/rebalance.py
@@ -1,7 +1,7 @@
 import numpy as np
 import features
 
-
+# Class rebalancing
 
 def balance_classes(X_train,y_train, positive_ratio=0.05):
 
@@ -22,14 +22,26 @@ def balance_classes(X_train,y_train, positive_ratio=0.05):
     indices = np.random.choice(len(negatives),n_negative,replace=False)
     return  np.concatenate((positives_x,negatives_x[indices])),np.concatenate((positives_y,negatives_y[indices]))
 
-#def remove_completely_black(X_train, y_train):
-    #feature_sums = X_train
+def remove_completely_black(X_train, y_train):
+    feature_sums = np.sum(X_train, axis=1)
+    print feature_sums
+
+    #Not fully black indices
+    indices_to_keep = np.where(feature_sums > 0)[0]
+
+    print "N feature vectors {0}, not fully black {1}".format(len(X_train),len(indices_to_keep))
+
+    return X_train[indices_to_keep], y_train[indices_to_keep]
+
 
 if __name__ == "__main__":
     print "Loading X"
     X_train, X_test = features.load_features()
     print "Loading Y"
     y_train, y_test = features.load_y()
+
+    print "Removing fully black features"
+    X_train, y_train = remove_completely_black(X_train, y_train)
 
     print "Balancing classes"
     X_train,y_train = balance_classes(X_train,y_train)
